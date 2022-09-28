8627250ed12b8deef8a352704ad6da5b7169b531
Uddhav Dahatonde <dahatondeuddhava@gmail.com>
2022-09-28 19:05:48 +0530

    new

--- Queue_impli.cs
+++ Queue_impli.cs
@@ -6,33 +6,33 @@ using System.Threading.Tasks;
 
 namespace _27septhomework
 {
-    public class Product
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int Quentity { get; set; }
-    }
-    internal class Queue_impli
-    {
-        static void Main(string[] args)
-        {
-           Queue<Product> queue = new Queue<Product>();
-            queue.Enqueue(new Product { Id=5,Name="pen",Quentity=50});
-            queue.Enqueue(new Product { Id = 7, Name = "NootBook", Quentity = 10 });
-            queue.Enqueue(new Product { Id = 10, Name = "Rabar", Quentity = 12 });
-            queue.Enqueue(new Product { Id = 2, Name = "Pencil", Quentity = 5 });
-            foreach (Product p in queue)
-            {
-                Console.WriteLine($"  {p.Id} {p.Name}  {p.Quentity}");
-            }
-            Console.WriteLine("____________________________________________");
-            queue.Dequeue();
-            queue.Dequeue();
-            foreach (Product p in queue)
-            {
-                Console.WriteLine($"  {p.Id} {p.Name}  {p.Quentity}");
-            }
+    //public class Product
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //    public int Quentity { get; set; }
+    //}
+    //internal class Queue_impli
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //       Queue<Product> queue = new Queue<Product>();
+    //        queue.Enqueue(new Product { Id=5,Name="pen",Quentity=50});
+    //        queue.Enqueue(new Product { Id = 7, Name = "NootBook", Quentity = 10 });
+    //        queue.Enqueue(new Product { Id = 10, Name = "Rabar", Quentity = 12 });
+    //        queue.Enqueue(new Product { Id = 2, Name = "Pencil", Quentity = 5 });
+    //        foreach (Product p in queue)
+    //        {
+    //            Console.WriteLine($"  {p.Id} {p.Name}  {p.Quentity}");
+    //        }
+    //        Console.WriteLine("____________________________________________");
+    //        queue.Dequeue();
+    //        queue.Dequeue();
+    //        foreach (Product p in queue)
+    //        {
+    //            Console.WriteLine($"  {p.Id} {p.Name}  {p.Quentity}");
+    //        }
 
-        }
-    }
+    //    }
+    //}
 }
