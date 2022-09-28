58201f4932bb55e641e1bed19d5ff911a7f26658
Uddhav Dahatonde <dahatondeuddhava@gmail.com>
2022-09-28 16:40:54 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27septhomework
+{
+    //public class Product
+    //{
+    //    //static int count;
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+
+    //    public int ProductPrice { get; set; }
+    //}
+    //internal class Program
+    //{
+    //    static void Main(string[] args)
+    //    { 
+            
+    //        List<Product> products = new List<Product>()
+    //        {
+    //            new Product{Id=1, Name="Dell Laptop",ProductPrice=60000},
+    //            new Product{Id=2, Name="Hp Laptop",ProductPrice=50000},
+    //            new Product{Id=3, Name="Panasonic Laptop",ProductPrice=550000},
+    //            new Product{Id=5, Name="Sony Laptop",ProductPrice=45000},
+
+    //        };
+    //        products.Add(new Product { Id = 6, Name = "Redmi", ProductPrice = 65000 });
+    //        products.Add(new Product { Id = 7, Name = "Vivo", ProductPrice = 55000 });
+    //        products.Insert(2, new Product { Id = 3, Name = "Samsung", ProductPrice = 80000 });
+    //        foreach (Product product in products)
+    //        {
+    //            Console.WriteLine($"  {product.Id}  {product.Name}  {product.ProductPrice}");
+    //        }
+    //        Console.WriteLine("--------------------------------------------------------------------");
+    //        products.Insert(2, new Product { Id = 3, Name = "Samsung", ProductPrice = 80000 });
+    //        foreach (Product product in products)
+    //        {
+    //            Console.WriteLine($"  {product.Id}  {product.Name}  {product.ProductPrice}");
+    //        }
+            
+    //        Console.WriteLine("---------------------------------------------------------------------");
+    //        products.Remove(products[2]);
+    //        foreach (Product product in products)
+    //        {
+    //            Console.WriteLine($"  {product.Id}  {product.Name}  {product.ProductPrice}");
+    //        }
+    //        Console.WriteLine("-------------------------------------------------------");
+    //        Console.WriteLine(products.Capacity);
+    //        products.Clear();
+    //        foreach (Product product in products)
+    //        {
+    //            Console.WriteLine($"  {product.Id}  {product.Name}  {product.ProductPrice}");
+    //        }
+    //    }
+    //}
+}
---
+++ Stack_.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27septhomework
+{
+    //public class Product
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //    public int Quentity { get; set; }
+    //}
+    //internal class Stack_
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Stack<Product> stack = new Stack<Product>();
+    //        stack.Push(new Product { Id = 1, Name = "Laptop", Quentity = 5 });
+    //        stack.Push(new Product { Id = 2, Name = "Tab", Quentity = 6 });
+    //        stack.Push(new Product { Id = 3, Name = "Pump", Quentity = 7 });
+    //        foreach (Product p in stack)
+    //        {
+    //            Console.WriteLine($"    {p.Id}   {p.Name}   {p.Quentity}");
+    //        }
+    //        Console.WriteLine("-------------------------------------------------");
+    //        stack.Pop();
+    //        foreach (Product p in stack)
+    //        {
+    //            Console.WriteLine($"    {p.Id}   {p.Name}   {p.Quentity}");
+    //        }
+    //        Console.WriteLine("-----------------------------------------------------");
+           
+            
+
+    //    }
+        
+	
+
+	//}
+   
+
+
+    
+
+	
+    
+}
